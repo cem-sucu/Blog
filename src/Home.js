d893e21a7c3837cff0dd9ce6bb480190d000945d
cem-sucu <cems.cems.17@gmail.com>
2022-07-06 11:28:24 +0200

    install react-router-dom

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,18 @@
 import BlogList from "./BlogList";
+import React from "react";
 import "./index.css";
 import useRecuperation from "./useRecuperation";
 
 const Home = () => {
-    const {
-        data: blogs,
-        isLoading,
-        error,
-    } = useRecuperation("http://localhost:8000/blogs");
+    const { data: blogs, isLoading, error } = useRecuperation(
+        "http://localhost:8000/blogs"
+    );
     return (
         <div className="home">
             {error && <div className="errMesssage">{error}</div>}
-            {isLoading && <div className="loadingMsg">En cours de traitement ... </div>}
+            {isLoading && (
+                <div className="loadingMsg">En cours de traitement ... </div>
+            )}
             {blogs && (
                 <BlogList
                     blogs={blogs}
